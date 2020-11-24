b6c8589baa81a5b851ba1641de9e252f906c22e6
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-11-24 18:15:02 +0200

    Add GET & POST Docs for Book

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -13,6 +13,10 @@ export default {
       {
         name: 'Status',
         description: 'Server Health Checkpoint'
+      },
+      {
+        name: 'Book',
+        description: 'Everything About Books'
       }
     ],
     paths: {
@@ -29,6 +33,78 @@ export default {
             404: {
               response: 'Not Found'
             }
+          }
+        }
+      },
+      '/books': {
+        get: {
+          tags: ['Book'],
+          summary: 'Returns a list of books',
+          consumes: ['application/json'],
+          produces: ['application/json'],
+          responses: {
+            200: {
+              description: 'OK'
+            },
+            404: {
+              description: 'Not Found'
+            },
+            500: {
+              description: 'Internal Server Error'
+            }
+          }
+        },
+        post: {
+          tags: ['Book'],
+          summary: 'Add book to database',
+          consumes: ['application/json'],
+          produces: ['application/json'],
+          parameters: [
+            {
+              in: 'body',
+              name: 'body',
+              description: 'Books object that needs to be added to the database',
+              required: true,
+              schema: {
+                $ref: '#/definitions/Book'
+              }
+            }
+          ],
+          responses: {
+            200: {
+              description: 'OK'
+            },
+            404: {
+              description: 'Not Found'
+            },
+            500: {
+              description: 'Internal Server Error'
+            }
+          }
+        }
+      }
+    },
+    definitions: {
+      Book: {
+        type: 'object',
+        properties: {
+          title: {
+            type: 'string'
+          },
+          author: {
+            type: 'string'
+          },
+          summary: {
+            type: 'string'
+          },
+          isbn: {
+            type: 'string'
+          },
+          genre: {
+            type: 'string'
+          },
+          uri: {
+            type: 'string'
           }
         }
       }
