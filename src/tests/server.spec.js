fa01e7f3a5e1f47b92b8b9c56d165286038fc1bd
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-11-24 15:09:32 +0200

    Add setter functions tests

--- src/tests/server.spec.js
+++ src/tests/server.spec.js
@@ -1,20 +1,77 @@
-import Server from '../models/classes/server';
+import compression from 'compression';
+import Server from '../classes/server';
+import statusRouter from '../routes/status';
 
-jest.mock('../models/classes/server');
+jest.mock('../classes/server');
 
-describe('Server Class', () => {
+describe('Server', () => {
+  let server;
+  const routes = [
+    {
+      name: 'status',
+      router(debug) {
+        return statusRouter(debug);
+      }
+    }
+  ];
+  const middlewares = [compression()];
+
   beforeEach(() => {
     Server.mockClear();
+    server = new Server(3000, 'localhost');
+    server.setMiddlewares(middlewares);
+    server.setApiRoutes(routes);
+    server.setSwagger();
+    server.start();
   });
 
-  it('Server Class Construcort Could be Called', () => {
-    // eslint-disable-next-line no-unused-vars
-    const server = new Server();
+  it('should call constructor method', () => {
     expect(Server).toHaveBeenCalledTimes(1);
   });
 
-  it('Server Constructor returns an Object', () => {
-    const server = new Server();
+  it('should return a server object', () => {
     expect(server).toBeInstanceOf(Object);
+  });
+
+  it('should call constructor method with port and host', () => {
+    expect(Server).toBeCalledWith(3000, 'localhost');
+  });
+
+  it('should call setMiddlewares', () => {
+    expect(server.setMiddlewares).toBeCalledTimes(1);
+  });
+
+  it('should call setMiddlewares with non empty middlewares array ', () => {
+    expect(middlewares.length).not.toBe(0);
+  });
+
+  it('should call setMiddlewares with array of functions', () => {
+    middlewares.forEach((middleware) => {
+      expect(typeof middleware).toBe('function');
+    });
+  });
+
+  it('should call setApiRoutes', () => {
+    expect(server.setApiRoutes).toBeCalledTimes(1);
+  });
+
+  it('should call setApiRoutes with non empty routes array ', () => {
+    expect(routes.length).not.toBe(0);
+  });
+
+  it('should call setApiRoutes with an array of objects ', () => {
+    routes.forEach((route) => {
+      expect(route).toBeInstanceOf(Object);
+      expect(typeof route.name).toBe('string');
+      expect(typeof route.router).toBe('function');
+    });
+  });
+
+  it('should call setSwagger', () => {
+    expect(server.setSwagger).toBeCalledTimes(1);
+  });
+
+  it('should call start', () => {
+    expect(server.start).toBeCalledTimes(1);
   });
 });
