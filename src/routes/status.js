516c07afe421e0d234d5f2a90dfc7502f357096d
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-11-24 02:53:34 +0200

    Create Health Check Point API

--- src/routes/status.js
+++ src/routes/status.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
-export default function statusRouter() {
+export default function statusRouter(logger, debug) {
   const router = new Router();
 
   router.get('/', (req, res) => {
+    debug.printSuccess(`${res.statusCode} ${req.method} ${req.originalUrl}`);
     res.send({ response: 'OK' });
   });
 
